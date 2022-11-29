d1ffb83657faf12a4b1ca05d840275b64e75a22d
Diego Prada <prada.gracia@gmail.com>
2022-11-29 16:18:11 -0600

    Adding first version of documentation

---
+++ lindelint/tests/test_import.py
@@ -0,0 +1,13 @@
+"""
+Unit and regression test for the lindelint package.
+"""
+
+# Import package, test suite, and other packages as needed
+import lindelint
+import pytest
+import sys
+
+def test_lindelint_imported():
+    """Sample test, will always pass so long as import statement worked"""
+    assert "lindelint" in sys.modules
+
